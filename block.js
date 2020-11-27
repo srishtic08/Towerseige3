73391c36b56b30a42f8739f129eb24f30f498507
srishtic08 <72651473+srishtic08@users.noreply.github.com>
2020-11-27 15:39:01 +0530

    Add files via upload

---
+++ block.js
@@ -0,0 +1,44 @@
+class Block{
+    constructor(x, y, width, height) {
+        var options = {
+           
+            restitution :0.4,
+            friction :0.0,
+            //isStatic:true
+           
+        }
+        this.Visiblity = 255
+        this.body = Bodies.rectangle(x, y, width, height, options);
+        this.width = width;
+        this.height = height;
+        World.add(world, this.body);
+      }
+      display(){
+        var angle = this.body.angle;
+        var pos= this.body.position;
+
+          console.log(this.body.speed);
+          if(this.body.speed < 3){
+            push();
+            translate(pos.x, pos.y);
+            rotate(angle);
+            rectMode(CENTER);
+            rect(0,0,this.width, this.height);
+         pop();
+          }
+          else{
+            World.remove(world, this.body);
+            push();
+            this.Visiblity = this.Visiblity - 5;
+            tint(255,this.Visiblity);
+           // rect(this.body.position.x, this.body.position.y, 50, 50);
+            pop();
+          }
+         }
+
+score(){
+  if(this.Visiblity<0 && this.Visiblity >-105){
+    score++;
+  }
+}
+}
